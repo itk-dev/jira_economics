4d8d5112e34fd77be218e64ee8dacc69563692d8
Jesper Pedersen <jesper@yepz.dk>
2019-03-26 13:30:52 +0100

    TEAMMANAGE-1318: Add new jira-economics symbol

--- assets/components/ItkLogo.js
+++ assets/components/ItkLogo.js
@@ -2,32 +2,12 @@ import React from 'react';
 
 export default function ItkLogo (props) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-         viewBox="0 0 24 24">
-      <g fill="none">
-        <polygon fill="#FECE60"
-                 points="10.294 10.294 21.647 10.294 15.867 21.647 10.294 21.647"
-                 transform="rotate(-135 15.97 15.97)"/>
-        <polygon fill="#7DBA6D"
-                 points="10.294 2.353 21.647 8.039 21.647 13.706 10.294 13.706"
-                 transform="rotate(-135 15.97 8.03)"/>
-        <polygon fill="#61B7E8"
-                 points="8.077 2.353 13.706 2.353 13.706 13.706 2.353 13.706"
-                 transform="rotate(-135 8.03 8.03)"/>
-        <polygon fill="#D56056"
-                 points="2.353 10.294 13.706 10.294 13.706 21.647 2.353 16.041"
-                 transform="rotate(-135 8.03 15.97)"/>
-        <polygon fill="#FECE60" points="7.941 16 14.765 16 13.347 18.765"
-                 transform="rotate(-135 11.353 17.382)"/>
-        <polygon fill="#22A136"
-                 points="15.94 7.941 13.588 8.748 15.941 16.059"/>
-        <polygon fill="#009BDD" points="13.177 5.237 10.705 5.928 13.351 13.177"
-                 transform="rotate(-89 12.028 9.207)"/>
-        <polygon fill="#C00122" points="10.411 8.001 8.001 8.867 10.527 16.001"
-                 transform="rotate(-179 9.264 12)"/>
-        <polygon fill="#FBB901"
-                 points="13.154 10.824 10.767 11.686 13.293 18.765"
-                 transform="rotate(91 12.03 14.795)"/>
+    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 415 411">
+      <g fill="#FFF" fill-rule="evenodd" transform="translate(0 -.466)">
+        <polygon fill-opacity=".8" points="198.672 185.448 294.131 187.685 371.894 224.671 294.106 378.461 198.672 378.448" transform="rotate(-135 285.283 281.954)"/>
+        <polygon fill-opacity=".9" points="225.673 43.518 380.153 119.347 380.153 215.681 187.153 215.681 187.153 119.885" transform="rotate(-135 283.653 129.6)"/>
+        <polygon points="120.104 33 215.8 33 215.8 226 120.717 226 43.838 187.681" transform="rotate(-135 129.82 129.5)"/>
+        <polygon fill-opacity=".9" points="34 197.263 227 197.263 226.763 292.722 188.205 368.242 34 294.959" transform="rotate(-135 130.5 282.752)"/>
       </g>
     </svg>
   );
