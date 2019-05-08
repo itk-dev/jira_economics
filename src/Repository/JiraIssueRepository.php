63ca329acacd17179865f6f8766fa2dab0f5f08b
Peter Bartram Hansen <hpeb@aarhus.dk>
2019-05-08 12:34:16 +0200

    SALG-739: Add repository for JiraIssue

---
+++ src/Repository/JiraIssueRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\JiraIssue;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method JiraIssue|null find($id, $lockMode = null, $lockVersion = null)
+ * @method JiraIssue|null findOneBy(array $criteria, array $orderBy = null)
+ * @method JiraIssue[]    findAll()
+ * @method JiraIssue[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class JiraIssueRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, JiraIssue::class);
+    }
+
+    // /**
+    //  * @return JiraIssue[] Returns an array of JiraIssue objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('j')
+            ->andWhere('j.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('j.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?JiraIssue
+    {
+        return $this->createQueryBuilder('j')
+            ->andWhere('j.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
