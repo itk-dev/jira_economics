b9e0abf9a3c10fdc9ae9336103d82b0d1b654be1
Peter Bartram Hansen <hpeb@aarhus.dk>
2019-05-10 10:39:21 +0200

    JE-4: Add missing bootstrap imports

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,8 @@
 import 'babel-polyfill';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
