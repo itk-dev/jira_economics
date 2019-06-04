2bb157d5637867ab51f6281ddad914fb6a943a80
Jesper Pedersen <jesper@yepz.dk>
2019-06-04 15:40:51 +0200

    JE-31: Add markup for billing homepage

--- bundles/Billing/Resources/assets/pages/HomePage.js
+++ bundles/Billing/Resources/assets/pages/HomePage.js
@@ -2,12 +2,104 @@ import React  from 'react';
 import ProjectList from '../components/ProjectList';
 import PageTitle from '../components/PageTitle';
 import ContentWrapper from '../components/ContentWrapper';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
+import Table from 'react-bootstrap/Table';
+import Form from 'react-bootstrap/Form';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Button from 'react-bootstrap/Button';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 export default function HomePage(props) {
   return (
     <ContentWrapper>
-      <PageTitle>ITK Jira projects</PageTitle>
-      <ProjectList/>
+      <div className="row">
+        <PageTitle className="col-sm-8">Fakturaer</PageTitle>
+        <div className="col-sm-4">
+          <Form className="float-right">
+            <Form.Group controlId="exampleForm.ControlSelect1">
+              <Form.Label>Sorter</Form.Label>
+              <Form.Control as="select">
+                <option>Nyeste først</option>
+                <option>Ældste først</option>
+              </Form.Control>
+            </Form.Group>
+          </Form>
+        </div>
+      </div>
+        <Tabs defaultActiveKey="drafts" id="uncontrolled-tab-example">
+          <Tab eventKey="drafts" title="Kladder">
+            <Table striped hover borderless>
+              <thead>
+                <tr>
+                  <th>Faktura navn</th>
+                  <th>Faktura dato</th>
+                  <th>Beløb (DKK)</th>
+                  <th className="float-right">Funktion</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td><strong>Udvikling sommer</strong></td>
+                  <td>30/09/2018</td>
+                  <td><strong>65.146</strong></td>
+                  <td className="float-right">
+                    <ButtonGroup size="sm" className="float-right" aria-label="Invoice functions">
+                      <Button className="btn-primary">rediger</Button>
+                      <Button className="btn-danger">slet</Button>
+                    </ButtonGroup>
+                  </td>
+                </tr>
+                <tr>
+                  <td><strong>Udvikling sommer</strong></td>
+                  <td>30/09/2018</td>
+                  <td><strong>65.146</strong></td>
+                  <td className="float-right">
+                    <ButtonGroup size="sm" className="float-right" aria-label="Invoice functions">
+                      <Button className="btn-primary">rediger</Button>
+                      <Button className="btn-danger">slet</Button>
+                    </ButtonGroup>
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+          </Tab>
+          <Tab eventKey="posted" title="Bogførte">
+            <Table striped hover borderless>
+              <thead>
+                <tr>
+                  <th>Faktura navn</th>
+                  <th>Faktura dato</th>
+                  <th>Beløb (DKK)</th>
+                  <th className="float-right">Funktion</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td><strong>Udvikling sommer</strong></td>
+                  <td>30/09/2018</td>
+                  <td><strong>65.146</strong></td>
+                  <td className="float-right">
+                    <ButtonGroup className="btn-group-sm float-right" aria-label="Invoice functions">
+                      <Button className="btn-outline-primary">hent csv</Button>
+                    </ButtonGroup>
+                  </td>
+                </tr>
+                <tr>
+                  <td><strong>Udvikling sommer</strong></td>
+                  <td>30/09/2018</td>
+                  <td><strong>65.146</strong></td>
+                  <td className="float-right">
+                    <ButtonGroup className="btn-group-sm float-right" aria-label="Invoice functions">
+                      <Button className="btn-outline-primary">hent csv</Button>
+                    </ButtonGroup>
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+          </Tab>
+        </Tabs>
     </ContentWrapper>
   )
 }
