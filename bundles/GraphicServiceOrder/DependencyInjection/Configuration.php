594e35e4a6d9a9528e2625e4ba5ac8bf6e28cc76
Martin Yde Granath <my@aarhus.dk>
2019-06-20 11:31:17 +0200

    JE-48: Added form

---
+++ bundles/GraphicServiceOrder/DependencyInjection/Configuration.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace GraphicServiceOrder\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('graphic_service_order');
+
+        return $treeBuilder;
+    }
+}
