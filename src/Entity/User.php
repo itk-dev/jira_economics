323c46ca324da1a356a72d18af24b7c523aa63e3
Mikkel Ricky <rimi@aarhus.dk>
2019-06-26 10:03:11 +0200

    JE-69: Cleaned up code

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file is part of aakb/jira_economics.
+ *
+ * (c) 2019 ITK Development
+ *
+ * This source file is subject to the MIT license.
+ */
 
 namespace App\Entity;
 
@@ -12,87 +19,87 @@ use FOS\UserBundle\Model\User as BaseUser;
  */
 class User extends BaseUser
 {
-  /**
-   * @ORM\Id
-   * @ORM\Column(type="integer")
-   * @ORM\GeneratedValue(strategy="AUTO")
-   */
-  protected $id;
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
 
-  /**
-   * @ORM\Column(name="jira_id", type="string", length=255, nullable=true)
-   */
-  private $jiraId;
+    /**
+     * @ORM\Column(name="jira_id", type="string", length=255, nullable=true)
+     */
+    private $jiraId;
 
-  private $jiraAccessToken;
+    private $jiraAccessToken;
 
-  /**
-   * @ORM\Column(type="json")
-   */
-  private $portals = [];
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $portals = [];
 
-  public function __construct()
-  {
-    parent::__construct();
-    // your own logic
-  }
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
 
-  public function setEmail($email)
-  {
-    $this->setUsername($email);
+    public function setEmail($email)
+    {
+        $this->setUsername($email);
 
-    return parent::setEmail($email);
-  }
+        return parent::setEmail($email);
+    }
 
-  /**
-   * @return mixed
-   */
-  public function getJiraId()
-  {
-    return $this->jiraId;
-  }
+    /**
+     * @return mixed
+     */
+    public function getJiraId()
+    {
+        return $this->jiraId;
+    }
 
-  /**
-   * @param mixed $jiraId
-   *
-   * @return User
-   */
-  public function setJiraId($jiraId)
-  {
-    $this->jiraId = $jiraId;
+    /**
+     * @param mixed $jiraId
+     *
+     * @return User
+     */
+    public function setJiraId($jiraId)
+    {
+        $this->jiraId = $jiraId;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  /**
-   * @return mixed
-   */
-  public function getJiraAccessToken()
-  {
-    return $this->jiraAccessToken;
-  }
+    /**
+     * @return mixed
+     */
+    public function getJiraAccessToken()
+    {
+        return $this->jiraAccessToken;
+    }
 
-  /**
-   * @param mixed $jiraAccessToken
-   *
-   * @return User
-   */
-  public function setJiraAccessToken($jiraAccessToken)
-  {
-    $this->jiraAccessToken = $jiraAccessToken;
+    /**
+     * @param mixed $jiraAccessToken
+     *
+     * @return User
+     */
+    public function setJiraAccessToken($jiraAccessToken)
+    {
+        $this->jiraAccessToken = $jiraAccessToken;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  public function getPortals(): ?array
-  {
-      return $this->portals;
-  }
+    public function getPortals(): ?array
+    {
+        return $this->portals;
+    }
 
-  public function setPortals(array $portals): self
-  {
-      $this->portals = $portals;
+    public function setPortals(array $portals): self
+    {
+        $this->portals = $portals;
 
-      return $this;
-  }
+        return $this;
+    }
 }
