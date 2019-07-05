35944b4a1556e3e24b43018fda7f5ce73386db15
Mikkel Ricky <rimi@aarhus.dk>
2019-07-05 15:57:20 +0200

    JE-77: Split Jira service into a service and a hammer

---
+++ src/Service/HammerService.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of aakb/jira_economics.
+ *
+ * (c) 2019 ITK Development
+ *
+ * This source file is subject to the MIT license.
+ */
+
+namespace App\Service;
+
+use GuzzleHttp\Client;
+
+class HammerService extends AbstractJiraService
+{
+    protected $apiUser;
+
+    protected $apiPass;
+
+    /**
+     * Constructor.
+     */
+    public function __construct(
+        $jiraUrl,
+        $apiUser,
+        $apiPass
+    ) {
+        parent::__construct($jiraUrl);
+        $this->apiUser = $apiUser;
+        $this->apiPass = $apiPass;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getClient()
+    {
+        return new Client(
+            [
+                'base_uri' => $this->jiraUrl,
+                'auth' => [
+                    $this->apiUser,
+                    $this->apiPass,
+                ],
+            ]
+        );
+    }
+}
