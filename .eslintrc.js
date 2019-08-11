15b00dd0e34f1a063a02c18cd6f4b26a24f0c1a0
Mikkel Ricky <rimi@aarhus.dk>
2019-08-11 13:25:33 +0200

    JE-110: Updated eslint parser and configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,17 @@
 module.exports = {
     "root": true,
-    "extends": "standard",
+    "extends": [
+        "standard",
+        "plugin:react/recommended"
+    ],
+    "settings": {
+        "react": {
+            "version": "detect"
+        }
+    },
+    // Needed to parse arrow functions as react class properties
+    // (e.g. `handleSubmit = (event) => { … }`.
+    "parser": "babel-eslint",
     "rules": {
         "indent": ["error", 4],
         // Party like it's 1999!
