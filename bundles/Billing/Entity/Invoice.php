e467ffc09e8c50cb74e6d72ad75894c4d8be96ba
Peter Bartram Hansen <hpeb@aarhus.dk>
2019-08-12 11:21:33 +0200

    JE-114: Allow description to be null

--- bundles/Billing/Entity/Invoice.php
+++ bundles/Billing/Entity/Invoice.php
@@ -34,7 +34,7 @@ class Invoice
     private $name;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $description;
 
