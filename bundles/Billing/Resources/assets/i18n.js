6675ec3f28393814b2d65e5779ee76d0530c64bb
Troels Ugilt Jensen <tujensen@gmail.com>
2019-12-12 13:54:25 +0100

    JE-277: Added translation

--- bundles/Billing/Resources/assets/i18n.js
+++ bundles/Billing/Resources/assets/i18n.js
@@ -129,6 +129,7 @@ const resources = {
             'invoice_entry.table.select_all': 'Vælg alle',
             'invoice_entry.table.deselect_all': 'Fravælg alle',
             'invoice_entry.form.helptext.product': 'Max 40 tegn.',
+            'invoice_entry.table.worklog_started': 'Dato',
             'invoice.form.type': 'Type',
             'invoice.form.types.manual': '',
             'invoice.form.types.expense': 'Udgift',
