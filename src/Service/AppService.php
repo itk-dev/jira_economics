32868cbdf123827923950bac1351906a5d468fee
Mikkel Ricky <rimi@aarhus.dk>
2019-12-18 14:25:09 +0100

    JE-272: Added check for Jira apps groups

--- src/Service/AppService.php
+++ src/Service/AppService.php
@@ -11,7 +11,9 @@
 namespace App\Service;
 
 use App\Entity\User;
+use HWI\Bundle\OAuthBundle\Security\Core\User\OAuthUser;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
@@ -29,11 +31,19 @@ class AppService
     /** @var \Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface */
     private $parameters;
 
-    public function __construct(TokenStorageInterface $tokenStorage, AuthorizationCheckerInterface $authorizationChecker, ContextService $contextService, ParameterBagInterface $parameters)
+    /** @var \App\Service\JiraService */
+    private $jiraService;
+
+    /** @var \Symfony\Component\HttpFoundation\Session\SessionInterface */
+    private $session;
+
+    public function __construct(TokenStorageInterface $tokenStorage, AuthorizationCheckerInterface $authorizationChecker, ContextService $contextService, JiraService $jiraService, SessionInterface $session, ParameterBagInterface $parameters)
     {
         $this->tokenStorage = $tokenStorage;
         $this->authorizationChecker = $authorizationChecker;
         $this->contextService = $contextService;
+        $this->jiraService = $jiraService;
+        $this->session = $session;
         $this->parameters = $parameters;
     }
 
@@ -65,19 +75,50 @@ class AppService
             if (null !== $token) {
                 /** @var \App\Entity\User $user */
                 $user = $token->getUser();
+                $enabledApps = [];
                 if ($user instanceof User) {
                     $enabledApps = $user->getPortalApps();
-                    $items = array_filter(
-                        $items,
-                        function ($app) use ($enabledApps) {
-                            return \in_array($app, $enabledApps);
-                        },
-                        ARRAY_FILTER_USE_KEY
-                    );
+                } elseif ($user instanceof OAuthUser) {
+                    $enabledApps = $this->getJiraApps($user);
                 }
+                $items = array_filter(
+                    $items,
+                    static function ($app) use ($enabledApps) {
+                        return \in_array($app, $enabledApps, true);
+                    },
+                    ARRAY_FILTER_USE_KEY
+                );
             }
         }
 
         return $items;
+    }
+
+    /**
+     * Get list of Jira apps a user has access to.
+     *
+     * @param \HWI\Bundle\OAuthBundle\Security\Core\User\OAuthUser $user
+     *
+     * @return array List of apps indexed by app id
+     */
+    private function getJiraApps(OAuthUser $user)
+    {
+        $cacheKey = __METHOD__.':'.$user->getUsername();
+
+        if (!$this->session->has($cacheKey)) {
+            $jiraUser = $this->jiraService->getCurrentUser(['expand' => 'groups']);
+            $userGroups = array_column($jiraUser->groups->items, 'name');
+            $jiraApps = array_filter(
+                $this->parameters->get('jira_app_groups'),
+                static function ($groups) use ($userGroups) {
+                    $groups = (array) $groups;
+
+                    return array_intersect($groups, $userGroups);
+                }
+            );
+            $this->session->set($cacheKey, array_combine(array_keys($jiraApps), array_keys($jiraApps)));
+        }
+
+        return $this->session->get($cacheKey);
     }
 }
