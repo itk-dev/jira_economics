06845e73d33b0eea00882bef270310c6e4a799a8
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2022-04-02 12:44:18 +0200

    JE-339: Fixed migration

--- src/Migrations/Version20220401065153.php
+++ src/Migrations/Version20220401065153.php
@@ -22,7 +22,6 @@ final class Version20220401065153 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE messenger_messages');
         $this->addSql('ALTER TABLE invoice ADD period_from DATETIME DEFAULT NULL, ADD period_to DATETIME DEFAULT NULL');
     }
 
@@ -31,7 +30,6 @@ final class Version20220401065153 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, headers LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, queue_name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('ALTER TABLE invoice DROP period_from, DROP period_to');
     }
 }
